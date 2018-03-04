4fe5d601e1aa147cf7dfb0c3cbf6fbaa25a39de9
Sophia Shapira <Sophia.Shapira@gmail.com>
2018-03-04 00:40:32 -0500

    Minor improvement to cover timestamp layout

--- prepare.sh
+++ prepare.sh
@@ -8,10 +8,11 @@ repodir="$(pwd)"
 rm -rf tmpres
 mkdir tmpres
 perl res/perlsplit.pl \
-"$(date '+%Y-%m-%d - %-I:%M%P::%Y-%m-%d-%H%M::%Y::%Y-%m-%d')" \
+"$(date '+%Y-%m-%d - %-I:%M%P::%Y-%m-%d-%H%M::%Y::%Y-%m-%d::%-I:%M%P')" \
 '::' \
 tmpres/datetime-neat.txt tmpres/datetime-cyber.txt \
-tmpres/year.txt tmpres/smdate.txt
+tmpres/year.txt tmpres/smdate.txt \
+tmpres/justhour.txt
 
 
 
@@ -20,8 +21,12 @@ tmpres/year.txt tmpres/smdate.txt
 
 rm -rf *.jpg *.png
 xcf2png CoverImage.xcf -o CoverImage.png
-convert CoverImage.png -gravity North -pointsize 105 -annotate +0+1800 "$(cat tmpres/datetime-neat.txt)" CoverImage01.png
-convert CoverImage01.png CoverImage.jpg
+convert CoverImage.png -gravity North -pointsize 120 -annotate +0+1700 "$(cat tmpres/smdate.txt)" CoverImage01.png
+convert CoverImage01.png -gravity North -pointsize 120 -annotate +0+1900 "$(cat tmpres/justhour.txt)" CoverImage02.png
+convert CoverImage02.png CoverImage.jpg
+
+
+
 
 rm -rf Images
 mkdir Images
